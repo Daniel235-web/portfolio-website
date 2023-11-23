9f3493def5c6fca01e5430d661f190554774a015
Daniel235-web <samueledaniel235@gmail.com>
2023-11-23 22:23:30 +0000

    header

---
+++ components/intro.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function intro() {
+  return (
+    <section>
+
+     <div className='flex items-center justify-center'>
+        <div>
+           <Image /> 
+        </div>
+    </div>   
+    </section>
+  )
+}
