87119b10ed5cbf94c57e9529e156599273e3a230
Daniel235-web <samueledaniel235@gmail.com>
2023-11-28 16:09:02 +0000

    fix issue

--- components/submit-btn.tsx
+++ components/submit-btn.tsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaPaperPlane } from "react-icons/fa";
-import { experimental_useFormStatus as useFormStatus } from "react-dom";
 
 export default function SubmitBtn() {
-  const { pending } = useFormStatus();
+  const [isPending, setPending] = useState(false);
+
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    // Set the form to a pending state
+    setPending(true);
+
+    try {
+      // Perform your form submission logic here
+      // For example, you can use fetch or axios to send the form data to a server
+
+      // Simulating a delay for demonstration purposes
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      // After submission is complete, reset the pending state
+      setPending(false);
+
+      // Additional logic after successful submission if needed
+    } catch (error) {
+      // Handle any errors during form submission
+  
+
+      // Reset the pending state in case of an error
+      setPending(false);
+    }
+  };
 
   return (
     <button
       type="submit"
+      onClick={handleSubmit}
       className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 dark:bg-white dark:bg-opacity-10 disabled:scale-100 disabled:bg-opacity-65"
-      disabled={pending}
+      disabled={isPending}
     >
-      {pending ? (
+      {isPending ? (
         <div className="h-5 w-5 animate-spin rounded-full border-b-2 border-white"></div>
       ) : (
         <>
